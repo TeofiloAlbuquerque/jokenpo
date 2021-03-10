98f53059d7cf13bce05e41b417352aff28897726
Theo Albuquerque <teofilogragas@gmail.com>
2021-03-10 12:27:07 -0300

    iniciando os trabalhos

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:jokenpo/jogo.dart';
+
+void main() {
+  runApp(MaterialApp(
+    // instanciando a class stf Jogo
+    home: Jogo(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
